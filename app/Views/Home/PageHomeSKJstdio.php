2778a27482816bce91940ee9b46ad6963bdfe2a6
dekpiano <dekpiano4815@gmail.com>
2023-11-27 12:56:15 +0700

    27/11/2566 เปลี่ยนหน้า วีดีโอโรงเรียนใหม่

--- app/Views/Home/PageHomeSKJstdio.php
+++ app/Views/Home/PageHomeSKJstdio.php
@@ -1,58 +1,65 @@
-<div class="wow fadeInUp mb-5 mt-5 bg-primary2" data-wow-delay="0.1s"
-    style="background-image:url(uploads/background/bg-video.webp);background-size: cover;">
+<style>
+.video {
+    background-image: url(uploads/background/bg-video1.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    padding: 100px 0px 250px 0px;
+    position: relative;
+}
 
-    <div class="d-flex">
-        <div class="me-5 w-50">
-            <iframe style="width:100%;height: 390px;" src="https://www.youtube.com/embed/RxdQ2-0scEQ" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen=""></iframe>
-        </div>
-        <div>
-            <h2 class="card-title display-4 font-weight-bold">
-                <a href="#" class="text-white" title="Blog title">
-                    SKJ Production
-                </a>
-            </h2>
-            <div class="card-text text-white mb-4">
-                <p class="lead">งานประชาสัมพันธ์ทางด้านมัลติมีเดีย</p>
-            </div>
-            <div class="mt-auto d-flex align-items-center pt-2">
+.section-heading h2 {
+    font-size: 55px;
+    font-weight: 700;
+    text-transform: capitalize;
+    margin-top: 20px;
+    line-height: 56px;
+    color: #fff;
+}
 
-                <div class="d-block text-white">
-                    <div class="font-weight-bold">งานโสต สวนจิ</div>
-                    <div class="text-grey">สวนกุหลาบ (จิรประวัติ) นครสวรรค์</div> <a href="https://skj.ac.th/Video"
-                        class="btn btn-primary mt-3">ดูทั้งหมด</a>
+.section-heading h6 {
+    color: #ee626b;
+    font-size: 15px;
+    text-transform: uppercase;
+    font-weight: 700;
+}
+.section-heading p{
+    color: #fff;
+}
+
+.video-content .video-frame {
+    position: relative;
+    box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);
+    border-radius: 10px;
+    margin-top: -216px;
+}
+</style>
+<div class="video section wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 offset-lg-4">
+                <div class="section-heading text-center">
+                    <h6>| งานโสต สวนจิ</h6>
+                    <h2>SKJ STUDIOS</h2>
+                    <p>งานประชาสัมพันธ์ทางด้านมัลติมีเดีย</p>
+                    <a href="https://www.youtube.com/@user-qb9js9ed6v" target="_blank"
+                        class="btn btn-primary mt-3">ติดตามช่อง Youtube</a>
                 </div>
             </div>
         </div>
     </div>
-    <!-- <div class="row">
-        <div class="col-6">
-                <iframe style="width:100%;height: 390px;" src="https://www.youtube.com/embed/B-hvfcpD4SY" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen=""></iframe>
-        </div>
-        <div class="col-6  p-5" style="">           
-            <h2 class="card-title display-4 font-weight-bold"> 
-                <a href="#" class="text-white" title="Blog title">
-                    SKJ   Production
-                </a> 
-            </h2>
-            <div class="card-text text-white mb-4">
-                <p class="lead">งานประชาสัมพันธ์ทางด้านมัลติมีเดีย</p>
-            </div>
-            <div class="mt-auto d-flex align-items-center pt-2">
-               
-                <div class="d-block text-white">
-                    <div class="font-weight-bold">งานโสต สวนจิ</div>
-                    <div class="text-grey">สวนกุหลาบ (จิรประวัติ) นครสวรรค์</div> <a href="https://skj.ac.th/Video"
-                        class="btn btn-primary mt-3">ดูทั้งหมด</a>
+</div>
+<div class="video-content wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1">
+                <div class="video-frame">
+                <iframe style="width:100%;height: 400px;" src="https://www.youtube.com/embed/RxdQ2-0scEQ" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen=""></iframe>
                 </div>
             </div>
         </div>
-    </div> -->
-
-
-
-
+    </div>
 </div>
+
